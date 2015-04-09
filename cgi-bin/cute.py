e8bf46662762873116ae415a9f53c542d8e714f7
Daniel Laikhter <daniellaikhter@gmail.com>
2015-04-08 23:39:27 -0400

    adding this bad boy to github

---
+++ cgi-bin/cute.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import urllib2, random, re, json
+from BeautifulSoup import BeautifulSoup
+
+while(True):
+	try:
+		html = BeautifulSoup(urllib2.urlopen('http://www.reddit.com/r/aww').read())
+	except:
+		pass
+	else:
+		break
+
+content = html.findAll('div', {'class':'entry unvoted'})
+links = [link.find('a') for link in content]
+
+animal_pics = [link['href'] for link in links if(re.search('i\.imgur' ,link['href']))]
+animal_pic = animal_pics[random.randint(0, len(animal_pics)-1)]
+
+phrases = ["you need to relax, here, have a cute animal!",
+	   "Trust me, you need this",
+	   "Here",
+	   "Oof, rough day huh?",
+	   "This one's for you",
+	   "Why don't you relax buddy",
+	   "Doctor! we need 50 cc's of cuteness stat",
+	   "awwww...",
+	   "What's this? cuteness!",
+	   "Look here and breath",
+	   "Somebody needs a break (it's you)",
+	   "Hang in there!",
+	   "D'aww...",
+	   "Error 477: user lacking happiness",
+	   "Only a few more hours and you're home free!",
+	   "We need to talk, but you should look at this instead",
+	   "Look at cute animal",
+	   "If you look in a mirror and you see a sad man, look at this",
+	   "I'm here for you",
+	   "Why don't you just relax and look at this cute animal",
+	   "Feel better?"]
+
+phrase = phrases[random.randint(0, len(phrases)-1)]
+response = {'image':animal_pic, 'phrase':phrase}
+
+print "Content-type: application/json"
+print 
+print(json.JSONEncoder().encode(response)) 
